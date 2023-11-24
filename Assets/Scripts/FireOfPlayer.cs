1c3d1dcf6eaac8451756ed60751358ad94f57bce
DONGxw813 <2083283315@qq.com>
2023-11-24 20:06:57 +0800

    上buff

--- Assets/Scripts/FireOfPlayer.cs
+++ Assets/Scripts/FireOfPlayer.cs
@@ -7,12 +7,15 @@ public class FireOfPlayer : MonoBehaviour
     public GameObject bulletPrefab;
     public Transform bulletFloder;
     public Transform firePoint;
-    public float fireInterval = 0.1f;
+    public float fireInterval;
+    private float _increasedFireSpeed = 1f;
+    public float initialFireInterval = 0.1f;
     
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("bulletFire", fireInterval, fireInterval);
+        fireInterval = initialFireInterval;
+        InvokeRepeating("BulletFire", fireInterval, fireInterval);
     }
 
     // Update is called once per frame
@@ -21,9 +24,15 @@ public class FireOfPlayer : MonoBehaviour
         
     }
 
-    private void bulletFire()
+    private void BulletFire()
     {
         GameObject node = Instantiate(bulletPrefab, bulletFloder);
         node.transform.position = firePoint.position;
+    }
+
+    public void IncreaseFire(float addedSpeed)
+    {
+        _increasedFireSpeed += addedSpeed;
+        fireInterval = initialFireInterval / 1 + _increasedFireSpeed;
     }
 }
--- Assets/Scripts/GetBuff.cs
+++ Assets/Scripts/GetBuff.cs
@@ -1,19 +1,29 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GetBuff : MonoBehaviour
 {
+    public float increaseBulletSpeed = 0.1f;
+    public UnityEvent<float> onGetBuff; 
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        onGetBuff.AddListener(GameObject.Find("Player").GetComponent<FireOfPlayer>().IncreaseFire);
+        onGetBuff.AddListener(F);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log(other.name + "碰到了" + this.name);
-        if (other.name == "Player") ;
+        if (other.name == "Player")
+        {
+            onGetBuff?.Invoke(increaseBulletSpeed);
+        }
+    }
+
+    private void F(float a)
+    {
+        Debug.Log("调用");
     }
 }
